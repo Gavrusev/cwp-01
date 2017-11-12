77b9fa95627bbe61d29019cf5622571e9249ce58
Aleksey Gavrusev <lexa1907_98@mail.ru>
2017-11-12 20:52:38 +0300

    task 3,4,5

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 const fs = require('fs');
 const path = require('path');
-function getFiles(dir) { 
-        fs.readdir(dir, function (err, files) { 
-            for (let i in files) { 
-                let currentDir = dir + path.sep + files[i]; 
-    
-                fs.stat(currentDir, (err, stats) => { 
-                    if (stats.isDirectory()) { 
-                        getFiles(currentDir); 
-                        return;
-                    }
-                    console.log(path.relative(__dirname, currentDir)); 
-                }); 
+const txt = 'txt';
+let copyright;
+let summary = 
+'const fs = require(\'fs\');\n'+
+'const path = require(\'path\');\n'+
+'function getFiles(dir) { \n'+
+'    fs.readdir(dir, function (err, files) {\n'+
+'        for (let i in files) { \n'+
+'            let currentDir = dir + path.sep + files[i];\n '+
+ '           fs.stat(currentDir, (err, stats) => { \n'+
+ '               if (stats.isDirectory()) { \n'+
+ '                   getFiles(currentDir);\n '+
+ '                   return;\n'+
+ '               }\n'+
+ '               console.log(path.relative(__dirname, currentDir));\n '+
+ '           });\n '+
+ '       }\n'+
+ '   });\n'+
+'};\n'+
+'getFiles(__dirname);\n';
+
+function createSummaryScript() {
+        fs.appendFile('summary.js', summary, (err) => {
+            if (err) {
+                console.log('summary.js not created');
             }
         });
-    };
+    }
     
-    getFiles(__dirname);
+ //createSummaryScript();
+
+ function setCopyright() {
+       fs.readFile("config.json", (err, data) => {
+            if (err) {
+                console.error("Not finded copyright file");
+                return;
+            }
+            copyright = JSON.parse(data);
+            console.log(copyright);
+        });
+    }
+    
+    //createSummaryScript();
+    
+    (() => {
+        setCopyright();
+    })();
